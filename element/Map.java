1149be62296051c9e4bf55a806d711ecb4552404
PotatoKingPotato <102517919+PotatoKingPotato@users.noreply.github.com>
2022-05-08 21:37:10 +0900

    SH_水正
    지역 허기
    오브젝트 하위 자원

--- element/Map.java
+++ element/Map.java
@@ -10,35 +10,41 @@ public class Map {
 class Grassland extends Map{
 	public Grassland() {
 		Map_Name = "Grassland";
+		Map_Down_hungry = 5;
 	}
 }
 
 class Forest extends Map{
 	public Forest() {
 		Map_Name = "Forest";
+		Map_Down_hungry = 10;
 	}
 }
 
 class Birchnut extends Map{
 	public Birchnut() {
 		Map_Name = "Birchnut";
+		Map_Down_hungry = 10;
 	}
 }
 
 class Savannah extends Map{
 	public Savannah() {
 		Map_Name = "Savannah";
+		Map_Down_hungry = 15;
 	}
 }
 
 class Swamp extends Map{
 	public Swamp() {
 		Map_Name = "Swamp";
+		Map_Down_hungry = 20;
 	}
 }
 
 class Rocky extends Map{
 	public Rocky() {
 		Map_Name = "Rocky";
+		Map_Down_hungry = 15;
 	}
 }
--- element/Object.java
+++ element/Object.java
@@ -1,14 +1,76 @@
 package element;
 
-public class Object {
+abstract public class Object {
 	String Obj_Name;
 	
-	String Drop_resource(){
-		return "나무";
-	};
+	abstract String Drop_resource();
 	
-	int Resource_Down_hungry(x) {
-		return Hungry - x;
-	};
+	abstract int Resource_Down_hungry();
+	
+}
+
+class Wood extends Object{
+	String Drop_resource() {
+		return "통나무";
+	}
+	int Resource_Down_hungry() {
+		return 10;
+	}
+}
+
+class Sapling extends Object{
+	String Drop_resource() {
+		return "잔가지";
+	}
+	int Resource_Down_hungry() {
+		return 5;
+	}
+}
+
+class Grass extends Object{
+	String Drop_resource() {
+		return "자른 풀";
+	}
+	int Resource_Down_hungry() {
+		return 5;
+	}
+}
+
+class Berry_bush extends Object{
+	String Drop_resource() {
+		return "베리";
+	}
+	int Resource_Down_hungry() {
+		return 5;
+	}
+}
+
+class Carrot_root extends Object{
+	String Drop_resource() {
+		return "당근";
+	}
+	int Resource_Down_hungry() {
+		return 5;
+	}
+}
+
+class Mushrrom extends Object{
+	String Drop_resource() {
+		return "버섯";
+	}int Resource_Down_hungry() {
+		return 5;
+	}
+	
+}
+
+class Rock extends Object{
+	
+	String Drop_resource() {
+		return "돌덩이";
+	}
+	
+	int Resource_Down_hungry() {
+		return 10;
+	}
 	
 }
