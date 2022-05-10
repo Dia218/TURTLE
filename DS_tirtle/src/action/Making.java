e1a56c54287b70eeb7319d46c52af2e778c45c77
honey720 <honey720@naver.com>
2022-05-10 20:54:33 +0900

    0510 이동헌 제작하기 수정

--- DS_tirtle/src/action/Making.java
+++ DS_tirtle/src/action/Making.java
@@ -5,5 +5,11 @@ public class Making {
 	/*
 	 * 도구 선택 - 제작 결과 출력 - 인벤토리에 수납
 	 */
+	private String ingredients[] = new String[4];
 	
+	public boolean chekcItem {
+		for(boolean check: ingredient) {
+			
+		}
+	}
 }
