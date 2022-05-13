92977f3e28ec0b90ec40562b573940a168f348a7
honey720 <honey720@naver.com>
2022-05-13 20:10:41 +0900

    0513 이동헌 아이템 이미지 추가 + 아이템 구조 수정+ 곤죽 추가

---
+++ DS_tirtle/src/item/processedFoods/WetGoop.java
@@ -0,0 +1,24 @@
+package item.processedFoods;
+
+import javax.swing.ImageIcon;
+
+public class WetGoop {
+	private static String name = "ฐ๏มื";
+	private static int starvePoint = 0;
+	private static int healPoint = 0;
+	
+	static private ImageIcon wetGoop = new ImageIcon("./images/Wet_Goop.png");
+	
+	public String returnName() {
+		return name;
+	}
+	public int returStarvePoint() {
+		return starvePoint;
+	}
+	public int returnHealPoint() {
+		return healPoint;
+	}
+	static public ImageIcon wetGoop() {
+		return wetGoop;
+	}
+}
