79505c115ab1fc5f54e47d2be802275f43706e7b
honey720 <honey720@naver.com>
2022-05-23 00:32:58 +0900

    0523 이동헌 요리하기 제작하기 더 만듬

--- DS_tirtle/src/action/ActionEventCheck.java
+++ DS_tirtle/src/action/ActionEventCheck.java
@@ -3,31 +3,44 @@ package action;
 import javax.swing.JButton;
 import user.*;
 
-public class ActionEventCheck {
-	//각 메뉴패널 버튼 체크 생성자
-	public ActionEventCheck(JButton clickButton) {
-
-		//방어구 만들기 버튼인 경우
-		if(clickButton.getText().equals("활동 끝내기") ) {
-			/*MakingArmors 클래스를 발생시켜서 방어구 만들기*/
-
-			Menu menu = new Menu();
-
-		}
-		else if(clickButton.getText().equals("방어구 만들기") ) {
-				/*MakingArmors 클래스를 발생시켜서 방어구 만들기*/
-
-			MakingArmors makingArmors = new MakingArmors();
-
-		}
-		//무기 만들기 버튼인 경우
-		else if(clickButton.getText().equals("무기 만들기") ) {
-				/*MakingWeapons 클래스를 발생시켜서 무기 만들기*/
+public class ActionEventCheck{
 	
-			MakingWeapons makingWeapons = new MakingWeapons(); 
+	//이벤트가 발생한 버튼을 확인하는 메소드
+		public void checkbutton(JButton clickButton) {
+			
+			new MakingEventCheck(clickButton);
+			new CookingEventCheck(clickButton);
+			//버튼을 체크하는 클래스들을 발생시킴
 		}
 		
-		else
-			;
-	}
+	//각 메뉴패널 버튼 체크 생성자
+	//public ActionEventCheck(JButton clickButton) {
+
+		//방어구 만들기 버튼인 경우
+		//if(clickButton.getText().equals("활동 끝내기") ) {
+			/*MakingArmors 클래스를 발생시켜서 방어구 만들기*/
+
+			//Menu menu = new Menu();
+			//System.out.println("활동 끝내기");
+		//}
+		//else if(clickButton.getText().equals("방어구 만들기") ) {
+				/*MakingArmors 클래스를 발생시켜서 방어구 만들기*/
+
+			//MakingArmors makingArmors = new MakingArmors();
+
+		//}
+		//무기 만들기 버튼인 경우
+		//else if(clickButton.getText().equals("무기 만들기") ) {
+				/*MakingWeapons 클래스를 발생시켜서 무기 만들기*/
+	
+			//MakingWeapons makingWeapons = new MakingWeapons(); 
+		//}
+		
+		//else
+			//;
+	//}
+	
+	
+	
+	
 }
